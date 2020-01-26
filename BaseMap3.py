469564abf1728af8a0863a9d76003151a04d2fe0
Hiram <svantelope@gmail.com>
2020-01-26 05:09:47 -0600

    Comentarios en el codigo

--- BaseMap3.py
+++ BaseMap3.py
@@ -12,24 +12,28 @@ from matplotlib.collections import PatchCollection
 from matplotlib.patches import PathPatch
 import numpy as np
 
+#Solo creamos plots que luego vamos a llenar
 fig     = plt.figure()
 ax      = fig.add_subplot(111)
-
+#Creamos nuestro mapa, entre los argumentos se encuentran: La calidad, proyeccion
+#Resolucion, centro y limites
 map = Basemap(resolution='i', # c, l, i, h, f or None
             projection='merc', 
             lat_0=14.6569, lon_0=-90.51,
             llcrnrlon=-92.93, llcrnrlat=13.15,urcrnrlon=-87.58, urcrnrlat=18.42)
 
+#Dibujamos el background de nuestro mapa
 map.drawmapboundary(fill_color='#46bcec')
 map.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
 map.drawcoastlines()
 
+#Leemos las Shapefiles. Estas las consegui en un rincon oscuro de internet, 
+#podemos generar las nuestras para pintar regiones, etc.
 map.readshapefile('Data/gtm/gtm_admbnda_adm0_ocha_conred_20190207', 'ej0',linewidth=1.0)
 map.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',drawbounds=False)
 map.readshapefile('Data/gtm/gtm_admbnda_adm2_ocha_conred_20190207', 'ej2',drawbounds=False)
 
 patches   = []
-
 #Pintamos solamente la capital
 for info, shape in zip(map.ej1_info, map.ej1):
     if info['ADM1_REF'] == 'Alta Verapaz':

a096df04d3a48bd550d4578f7c9aed5491a348a4
Analista2 <rt@analista2.local>
2020-02-20 05:04:29 -0600

    Pruebas adicionales: Mascara de solo en municipio de Guatemala

--- BaseKry.py
+++ BaseKry.py
@@ -29,20 +29,20 @@ from matplotlib.collections import PatchCollection
 
 # Leer un archivo
 #---------------------**----------------------
-
-datafile='eventos/2019-07-31-0555.csv'
-df=pd.read_csv(datafile)
-lons=np.array(df['lon']) 
-lats=np.array(df['lat']) 
-data=np.array(df['int_i'])
+#
+#datafile='eventos/2019-07-31-0555.csv'
+#df=pd.read_csv(datafile)
+#lons=np.array(df['lon']) 
+#lats=np.array(df['lat']) 
+#data=np.array(df['int_i'])
 
 #Leer el registro con Geophysics
 #---------------------**----------------------
-"""
 X0 = []
 X1 = []
 y = []
-fac02 = fac02 = [('ACRIS', 3.382968404),('CAUST', 3.615230044), ('CDBOS',1.378325942), ('CSTER', 2.060144124), ('EXCEL', 1.749653548),('ITC', 1.769262749), ('JUAMA',2.919761641), ('LSECB', 2.582940687), ('RGIL', 1.6264551), ('SEUNI',2.027508315), ('SJPIN',1.71722561),('CCONS',1)]
+fac02 =  [('ACRIS', 1206.1448),('CAUST', 1182.828208), ('CDBOS',1179.0282), ('EXCEL', 1180.7432), ('ITC', 1184.6142), ('LSECB', 1190.994),  ('SJPIN',1180.5178),('CCONS',1178.7244)]
+#fac02 = [('ACRIS', 3.686681571),('CAUST', 5.928297668), ('CDBOS',1.487064836), ('CSTER', 2.996806132), ('ITC', 3.461992973),('KINAL', 1.954327691), ('LSECB', 3.970137336), ('RGIL', 2.407856915), ('TADEO',1.840146918), ('SJPIN',4.68141169),('CCONS',1)]
 for i in fac02:
     a = estacion(i[0])
     y.append(i[1])
@@ -52,14 +52,15 @@ for i in fac02:
 #---------------------**----------------------
 lons=np.array(X0) 
 lats=np.array(X1) 
-data=np.array(y) """
+data=np.array(y)
 
 #Creando un Grid con maximos/minimos o valores especificos
 grid_space = 0.01
 #llcrnrlon=-90.59, llcrnrlat=14.53,urcrnrlon=-90.346, urcrnrlat=14.73
-grid_lon = np.arange(np.amin(lons), np.amax(lons), grid_space) #grid_space is the desired delta/step of the output array 
+#grid_lon = np.arange(np.amin(lons), np.amax(lons), grid_space) #grid_space is the desired delta/step of the output array 
+grid_lon = np.arange(np.amin(lons)-1, np.amax(lons)+0.3, grid_space)
 #grid_lon = np.arange(-90.59, -90.346, grid_space) #grid_space is the desired delta/step of the output array 
-grid_lat = np.arange(np.amin(lats), np.amax(lats), grid_space)
+grid_lat = np.arange(np.amin(lats)-1, np.amax(lats)+0.3, grid_space)
 #grid_lat = np.arange(14.53, 14.73, grid_space)
 
 #Instancia Kriging Ordinario
@@ -83,7 +84,9 @@ m = Basemap(resolution='i', # c, l, i, h, f or None
 m.drawcoastlines() #draw coastlines on the map
 m.readshapefile('Data/gtm/gtm_admbnda_adm0_ocha_conred_20190207', 'ej0',linewidth=1.5)
 m.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',linewidth=1.5,drawbounds=False)
-#m.readshapefile('Data/gtm/gtm_admbnda_adm2_ocha_conred_20190207', 'ej2',linewidth=0.5)
+m.readshapefile('Data/gtm/gtm_admbnda_adm2_ocha_conred_20190207', 'ej2',linewidth=0.5,drawbounds=False)
+#m.readshapefile('Qgis/Zonas', 'ej3',linewidth=2)
+#print(m.ej2_info)
 """
 patches   = []
 #Pintamos solamente la capital
@@ -92,20 +95,22 @@ for info, shape in zip(m.ej1_info, m.ej1):
         patches.append( Polygon(np.array(shape), True) )
         
 ax.add_collection(PatchCollection(patches, facecolor= 'm', edgecolor='k', linewidths=1., zorder=2))
-"""
+#"""  
 #Colocamos solo la capital
-for info, shape in zip(m.ej1_info, m.ej1):
-    if info['ADM1_REF'] == 'Guatemala':
+#"""
+for info, shape in zip(m.ej2_info, m.ej2):
+    if info['ADM2_REF'] == 'Guatemala':
         x, y = zip(*shape) 
         m.plot(x, y, marker=None,color='k')
-#"""        
+#"""   
 x,y=m(xintrp, yintrp) # convert the coordinates into the map scales
 ln,lt=m(lons,lats)
 
 #Normalizamos la data para colorear
 norm = mpl.colors.Normalize(vmin=min(data), vmax=max(data))
 cmap1 = cm.jet
-cs=ax.contourf(x, y, z1, np.linspace(0,5,51),extend='both',cmap=cmap1) #plot the data on the map.
+mc = cm.ScalarMappable(norm = norm, cmap = cmap1)
+cs=ax.contourf(x, y, z1, np.linspace(min(data),max(data)+0.1,100),extend='both',cmap=cmap1, alpha = 1) #plot the data on the map.18
 cbar=m.colorbar(cs,location='right',pad="7%") #plot the colorbar on the map
 
 # draw parallels.
@@ -126,7 +131,7 @@ map_edges = np.array([[x0,y0],[x1,y0],[x1,y1],[x0,y1]])
 ##getting all polygons used to draw the coastlines of the map
 polys = [p.boundary for p in m.landpolygons]
 print("Polys 1:")
-print(polys)
+#print(polys)
 ##combining with map edges
 #polys = [map_edges]+polys[:]
 #print("Polys 2:")
@@ -134,20 +139,26 @@ print(polys)
 
 patches   = []
 patches2   = []
+patches3   = []
 #Pintamos solamente la capital
 for info, shape in zip(m.ej1_info, m.ej1):
     if info['ADM1_REF'] == 'Guatemala':
         patches.append(np.array(shape))
 print("Patches: ")        
-print(patches)
+#print(patches)
 #print(m.ej0_info)
 for info, shape in zip(m.ej0_info, m.ej0):
     if info['ADM0_ES'] == 'Guatemala':
         patches2.append(np.array(shape))
 print("Patches: ")        
-print(patches2)
+#print(patches2)
+for info, shape in zip(m.ej2_info, m.ej2):
+    if info['ADM2_ES'] == 'Guatemala':
+        patches3.append(np.array(shape))
+print("Patches: ")        
+#print(patches2)
 
-polys = [map_edges]+patches
+polys = [map_edges]+patches3
 ##creating a PathPatch
 codes = [
         [Path.MOVETO]+[Path.LINETO for p in p[1:]] 
@@ -165,7 +176,7 @@ patch = PathPatch(path,facecolor='white', lw=0,alpha=1.0)
 ax.add_patch(patch)
 #"""
 #Titulo
-plt.title("Mapas SSG")
+plt.title("Mapa de Espectro de aceleraciones en PGA")
 #Guardar Imagen
 #plt.savefig('Imagenes/EstacionesNacional.png', bbox_inches='tight')
 plt.show()

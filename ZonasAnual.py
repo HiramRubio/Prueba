ebb2fe0926f8be285a82c1a5b8140aa1f510b39c
HiramRV <rt@analista2.local>
2020-03-05 06:31:22 -0600

    Agregando comentarios en el código

--- ZonasAnual.py
+++ ZonasAnual.py
@@ -6,27 +6,21 @@ Created on Thu Mar  5 05:00:15 2020
 @author: Steven Rubio
 """
 
-import sys
-import copy
+
 import time
-import random
-
 from pointInside import *
-
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import PathPatch
 import numpy as np
 import pandas as pd
-import time
 #-------------------------------------------------
 #Solo creamos plots que luego vamos a llenar 
 fig     = plt.figure()
 ax      = fig.add_subplot(111)
 #Creamos nuestro mapa, entre los argumentos se encuentran: La calidad, proyeccion
 #Resolucion, centro y limites
+#Leemos los datos y delimitamos los limites de nuestro mapa
 dfs = pd.read_csv('Data/Anual2019.csv')
 #print(max(dfs[' lon']),min(dfs[' lon']))
 x1,x2 = max(dfs[' lon']),min(dfs[' lon'])
@@ -50,55 +44,50 @@ map.readshapefile('Data/gtm/gtm_admbnda_adm2_ocha_conred_20190207', 'ej2',drawbo
 map.readshapefile('Data/ale/ZONASSISMOMOD', 'ej3',drawbounds=True)
 #print(map.ej3_info)
 
-patches   = []
-patches2 = []
-#Pintamos solamente la capital
-for info, shape in zip(map.ej3_info, map.ej3):
-    if info['SHAPENUM'] == 3:
-        patches.append( Polygon(np.array(shape), True))
-        patches2.append(np.array(shape))
-
+#Definimos los nombres de las zonas y el color que le queremos asignar a cada una
 Zonas = ['G1','S1','G2-S2','S3','G3','G4','G5-S4-H1','G6','G8']
 Colors = ['b','w','r','c','m','y','k','g','#cc66ff']
 patches3 = []
 
+#Tiempo 0 de ejecucion del programa
 t = time.perf_counter() 
-
+#Creamos la lista de los datos del nuevo csv
 n_dat = []
+#Recorremos todas las shapes de ej3, estas son las zonas sismogenicas de GT
 for info, shape in zip(map.ej3_info, map.ej3):
+    #Hacemos un ciclo para corroborar si un sismo esta en cada zona
     for j in range(len(Zonas)):
         if info['ZONA'] == Zonas[j]:
             x, y = zip(*shape)
         ax.plot(x, y, color='k') 
         for i in range(len(x)):
             patches3.append((x[i],y[i]))
-        
+            
+        #Esta lista nos servira para eliminar los sismos  ya clasificados
         rm = []
-        #print(dfs)
-        #print("*********************")
-        #print(n_dat)
         if(len(dfs)>0):
+            #Recorremos todo el listado de sismos
             for i in range(len(dfs)):
-                #print("Longitud: ",len(dfs))
-                #print(i)
                 xpo = dfs[' lon'][i]
                 ypo = dfs[' lat'][i]
                 xpt,ypt = map(xpo,ypo)
                 testP = (xpt,ypt)
+                #Si las coordenadas del punto están dentro de la Zona a prueba
+                #Se entra a este if
                 if (is_inside_sm(patches3,testP)):
                     ax.plot(xpt,ypt,'o',color =Colors[j])
                     var = dfs[' folder'][i]
-                    #print(xpo,ypo,var,j)
+                    #Adjuntamos la data de interes y el indice que vamos a eliminar
                     n_dat.append((xpo,ypo,var,j))
-                    #print("La estacion", dfs['Name'][i], "Esta dentro de la region: ",Zonas[j]) 
-                    #print(dfs.index[i-1])
                     rm.append(dfs.index[i])
-                
+            #Eliminamos los sismos ya clasificados y reiniciamos el index de nuestro archivo
             dfs.drop(rm,inplace =True)   
             dfs.reset_index(drop=True, inplace=True)
             
         patches3 = []
-
+        
+#Aqui estamos asignando la denominacion SC = Sin clasificar a todos los mismos
+#Que no pertenecen a las zonas sismogenicas de GT
 if(len(dfs)>0):
     rm = []
     for i in range(len(dfs)):
@@ -111,13 +100,14 @@ if(len(dfs)>0):
         n_dat.append((xpo,ypo,var,'SC'))
         rm.append(dfs.index[i])      
     dfs.drop(rm,inplace =True)             
-                
+
+#Verificamos que todos los sismos se encuentren clasificados            
 print("Longitud: ",len(dfs))
+#Mostramos el tiempo de ejecucion
 print("Execution time: " + str(time.perf_counter() - t)) 
 
-#print(n_dat)
+#Creamos un nuevo csv con la informacion que necesitamos
 dfs_n = pd.DataFrame(n_dat,columns=['lon', 'lat', 'folder','Zona'])
-#print(dfs_n)
 dfs_n.to_csv('Data/Anual2019_M.csv',index=True)
-
+#Mostramos la data y las regiones
 plt.show()

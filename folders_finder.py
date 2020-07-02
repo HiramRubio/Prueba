d90520c3135291b65d428ee6efa51fcb04db15db
HiramRubio <stevenrv97@gmail.com>
2020-07-02 04:50:19 -0600

    Update
    Comentaios en el código

--- folders_finder.py
+++ folders_finder.py
@@ -23,11 +23,12 @@ def zone_finder(dfs,column):
     # Eventos   = Lista con los eventos clasificados
     # ----------    
     
-    name    = Evento
+
     #Columnas con las que el método funciona
     opc_val = ['Zona','Zona2']
     #Lista de eventos
     Eventos = []
+    #Columnas disponibles en el csv
     x = dfs.columns
     #Validación de que el archivo que estamos utilizando contiene la clasificación zone
     if( column in x and column in opc_val):
@@ -39,34 +40,41 @@ def zone_finder(dfs,column):
         opc_names = np.unique(zones)
         var = True
         #Ciclo para seleccionar zona o zonas
+        
         print('Opciones Disponibles')
         print(opc_names)
         while(var):
+            #Entrada dle usuario
             a = input('Ingrese la zona a clasificar, Done, Zonas o Selec (Seleccionados): ')
+            #Entrada No Valida: El usuario intenta ingresar más zonas de las presentadas, automáticamente
+            #sale del ciclo con todas las opciones
             if(len(opc)==len(opc_names)):
                 print('Todas las Zonas seleccionadas, ejecutando busqueda')
                 var = False
+            #Salida del ciclo, esta es válida si se tiene al menos una opcion seleccionada
             if(a == 'Done'): 
                 if(len(opc)== 0): 
                     print('Ninguna Zona seleccionada')
                 else: 
                     var = False  
+            #Impresión de Zonas disponibles para clasificcion
             if(a == 'Zonas'):   
                 print(opc_names)
-                pass
-            
+            #Impresión de Zonas seleccionadas hasta el momento
             if(a == 'Selec'):   
                 print(opc)
             else:
                 if(a in opc_names): 
+                    #Entrada no Valida: Una opcion repetida
                     if(a in opc):   print('Opcion ya seleccionada')
                     else:           opc.append(a)
                 else: 
+                    #Entrada no valida: Una opcion no valida, entre otras
                     if(a!='Zonas' and len(opc)!=len(opc_names)): print('Opcion no valida')
                 
         print('Zonas seleccionadas')
         print(opc)
-        #Nombre de los eventos
+        #Nombre de los eventos del csv
         names  = dfs['folder']
         i = 0
         #Buscamos los folders que cuenten con 
@@ -74,11 +82,16 @@ def zone_finder(dfs,column):
             if(b in opc): 
                 Eventos.append(a)
                 i = i+1
+                
+        #Conteo de eventos
         print("Cantidad de eventos localizados: "+str(i))
+        #Retornamos los eventos que cumplieron
         return(Eventos)
         
         
     else:
+        #En caso de que el archivo no conenta la columna que especificamos
+        #Returnomos un False
         print('Archivo no contiene columna Zona ')
         return(False)
     

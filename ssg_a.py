92d696c779d1f47c1f6f58a23e39fb625a4340ab
HiramRubio <stevenrv97@gmail.com>
2020-07-10 06:59:55 -0600

    Super Update
    - Expansión del menu y de opciones disponibles

--- ssg_a.py
+++ ssg_a.py
@@ -14,14 +14,18 @@ from folders_finder import *
 from stations_extractor import * 
 
 #Ejecución del programa
-
 #Consola para imprimir mensajes en pantalla. 
 console = Console()
 
-name = 'Prueba_0_prof'
+#Variables Iniciales/Globales
+name = ''
+dfs = 'NONE'
+Eventos = []
+homeDir = "C:/Users/HRV/Desktop/Post-U/Trabajo/Prueba/Data/Eventos/"
+EventosD = ["2019-03-05-1315",'2019-11-14-0647','2020-03-06-0125']
 
+'''
 if(False):
-    #Eventos = ["2019-03-05-1315",'2020-03-15-0122',"2020-04-22-2322","2020-04-07-1102","2020-04-07-1122","2019-05-12-2356","2019-05-13-0150"]
     EventosD = ["2019-03-05-1315",'2019-11-14-0647','2020-03-06-0125']
     homeDir = "C:/Users/HRV/Desktop/Post-U/Trabajo/Prueba/Data/Eventos/"
 else:
@@ -32,11 +36,120 @@ else:
     for envt in Env_names:
         Eventos.append(envt)
     homeDir = '/antelope/analysis/eventos/'
-
+'''
 #Verificamos que nuestro programa sea principal
 if __name__ == "__main__":  
-    #Leemos un archivo con todos los filtros
-    events = []
-    dfs = pd.read_csv('Data/Informe2A.csv')
-    Eventos = range_finder(dfs,'prof',events)
-    events_station_extractor(Eventos,name,homeDir)
+    #Menu con todas las opciones
+    ACTIVE = True
+    console.print('¡Bienvenido! Elige un [cyan]numero[/cyan] de las siguiente opciones:')
+    while(ACTIVE):
+        #Opciones
+        console.print('1.   Extraer estaciones en base a un archivo')
+        console.print('2.   Extraer estaciones de un evento')
+        console.print('0.   [red]Salir[/red]')
+        
+        #Entrada del usuario
+        a = input()
+        
+        #Verificación de entrada valida
+        try:
+            a = int(a)
+        #Manejo de entrada incorrecta
+        except ValueError:
+            console.print('Entrada [red]NO[/red] valida')
+            
+        #Salida del Menu
+        if(a == 0):
+            ACTIVE = False
+        #events = []
+        
+        #OPCION 1
+        if(a ==1):
+            ACTIVE2 = True
+            while(ACTIVE2):
+                console.print('1.   Seleccionar archivo')
+                console.print('2.   Filtrar ')
+                console.print('3.   Mostrar eventos seleccionados ')
+                console.print('4.   Generar Archivo ')
+                console.print('0.   [red]Regresar[/red]')
+                
+                        #Entrada del usuario
+                b = input()
+                
+                #Verificación de entrada valida
+                try:
+                    b = int(b)
+                #Manejo de entrada incorrecta
+                except ValueError:
+                    console.print('Entrada [red]NO[/red] valida')
+                    
+                #Salida del Menu
+                if(b == 0):
+                    ACTIVE2 = False
+                    
+                #Opcion 1-1   
+                #Lectura de un archivo Pre-Procesado
+                if(b == 1):
+                    dfs = pd.read_csv('Data/Informe2A.csv')
+                    console.print('Archivo Seleccionado: [blue]Informe2A[/blue]')
+                
+                #OPCION 1-2 No valida
+                if(b == 2 and type(dfs) == str):
+                    console.print('Archivo para trabajar aún [red]no[/red] selecionado')
+                    
+                #OPCION 1-2
+                if(b ==2 and type(dfs) != str):
+                    ACTIVE3 = True
+                    while(ACTIVE3):
+                        console.print('1.   Profundiad')
+                        console.print('2.   Magnitud ')
+                        console.print('3.   Zona SMG ')
+                        console.print('4.   Zonas Profundidad ')
+                        console.print('0.   [red]Regresar[/red]')
+                        
+                        #Entrada del usuario
+                        c = input()
+                        
+                        #Verificación de entrada valida
+                        try:
+                            c = int(c)
+                        #Manejo de entrada incorrecta
+                        except ValueError:
+                            console.print('Entrada [red]NO[/red] valida')
+                            
+                        #Salida del Menu
+                        if(c == 0):
+                            ACTIVE3 = False
+                        #Filtro por protundidad
+                        if(c == 1):
+                            Eventos = range_finder(dfs,'prof',Eventos)
+                        #Filtro por magnitud
+                        if(c == 2):
+                            Eventos = range_finder(dfs,'ml',Eventos)
+                        #Filtro por zonas sismogeneticas
+                        if(c == 3):
+                            Eventos = zone_finder(dfs,'Zona',Eventos)
+                        #Filtro por zonas de profundidad
+                        if(c == 4):
+                            Eventos = zone_finder(dfs,'Zona2',Eventos)
+                
+                #Opcion 1-3
+                if(b == 3 and len(Eventos)!=0):
+                    console.print(Eventos)
+                    
+                #Opcion 1-4 No valida
+                if(b == 4 and len(Eventos)==0):
+                    console.print('Eventos [red]no[/red] selecionados')
+                    
+                #Opcion 1-4
+                if(b == 4 and len(Eventos)!=0):
+                    console.print('Ingresar nombre del Archivo: ')
+                    name = input()
+                    events_station_extractor(Eventos,name,homeDir)
+                    
+                    
+    console.print('Adios!', style="bold blue")
+    #dfs = pd.read_csv('Data/Informe2A.csv')
+    #Eventos = range_finder(dfs,'prof',events)
+    #events_station_extractor(Eventos,name,homeDir)
+    

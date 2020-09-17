5f38aced8dd75a61caad8a8dae13edb096d80133
HiramRubio <stevenrv97@gmail.com>
2020-09-17 05:47:42 -0600

    Update
    - Más comentarios
    - Fondo diferente
    - Estaciones inicializadas con el borde.

--- AnimatedTry.py
+++ AnimatedTry.py
@@ -5,12 +5,11 @@ Created on Sun Sep  6 02:48:57 2020
 @author: Hiram
 
 Aprendiendo a hacer imagenes animadas
+de las estaciones que detectaron un evento en específico
 """
-
 
 import pandas as pd
 import matplotlib.pyplot as plt
-#from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 from mpl_toolkits.basemap import Basemap
 import math
@@ -21,18 +20,20 @@ fig, ax = plt.subplots(figsize=(8,8))
 #Nacional
 m = Basemap(resolution='i', # c, l, i, h, f or None
     lat_0=14.6569, lon_0=-90.51,
-    llcrnrlon=-92.93, llcrnrlat=13.15,urcrnrlon=-87.58, urcrnrlat=18.42,
+    llcrnrlon=-92.93, llcrnrlat=13.15,urcrnrlon=-87.58, urcrnrlat=18.42,epsg=4326,
     projection='tmerc')
-    
-m.drawmapboundary(fill_color='#46bcec')                  
-m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
+
+#Cargamos el "fondo# del mapa
+m.arcgisimage(service='World_Terrain_Base', xpixels=1600, dpi=210,verbose= True)    
+#m.drawmapboundary(fill_color='#46bcec')                  
+#m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
 #Leemos nuestra shapefile, no los activamos todos
 m.readshapefile('Data/gtm/gtm_admbnda_adm0_ocha_conred_20190207', 'ej0',linewidth=1.5)
 m.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',linewidth=0.5)
 
 #Data de cada estacion 
-#Evento 2019-11-13-1628
-est = pd.read_csv('Data/g1.csv',sep=';',names=['num1','Folder','Prof','mag','Est','Lat','Lon','Dist','Angle','Onda','DeltaT'])
+#Evento 2019-11-13-1628, 2020-06-09-0514
+est = pd.read_csv('Data/G2.csv',sep=';',names=['num1','Folder','Prof','mag','Est','Lat','Lon','Dist','Angle','Onda','DeltaT'])
 #Directorio con data del evento
 homeDir = "C:/Users/HRV/Desktop/Post-U/Trabajo/Prueba/Data/Eventos/"
 #Data principal del evento
@@ -44,13 +45,17 @@ t_max =math.ceil(max(est['DeltaT']))+2
 # initialization function 
 def init(): 
     #Texto/Leyenda
-    ax.plot(420000,400000,'s',marker='^',color='r', markersize=6)
-    ax.plot(420000,410000,'s',marker='^',color='g', markersize=6)
-    ax.text(430000,395000,'Onda P', fontsize=8)
-    ax.text(430000,405000,'Onda S', fontsize=8)
+    ax.plot(-89.00,17.0,'s',marker='^',color='r', markersize=6)
+    ax.plot(-89.00,16.8,'s',marker='^',color='g', markersize=6)
+    ax.text(-88.90,16.97,'Onda P', fontsize=8)
+    ax.text(-88.90,16.77,'Onda S', fontsize=8)
     #Plot epicentro
     x,y = m(float(data[1]),float(data[0]))
-    plot = ax.plot(x,y,'s',marker='*',color='k', markersize=10)
+    plot = ax.plot(x,y,'s',marker='*',color='k', markersize=10,markeredgecolor = 'k')
+    #Plot de las estaciones sin color
+    for lat,lon in zip(est['Lat'],est['Lon']):
+        x,y = m(lon,lat)
+        plot = ax.plot(x,y,'s',marker='^',color='none', markersize=10,markeredgecolor = 'k')
     return plot
 
 # Estaciones a plotear
@@ -78,12 +83,12 @@ def animate(i):
     #Buscamos en que estaciones aparecen las ondas P
     xp,yp = station_check(ploted,i,'P')           
     #Actualización de plot              
-    plot = ax.plot(xp,yp,'s',marker='^',color='r', markersize=10)
+    plot = ax.plot(xp,yp,'s',marker='^',color='r', markersize=10,markeredgecolor = 'k')
     
     #Buscamos en que estaciones aparecen las ondas S
     xp,yp = station_check(ploted,i,'S')           
     #Actualización de plot              
-    plot = ax.plot(xp,yp,'s',marker='^',color='g', markersize=6)
+    plot = ax.plot(xp,yp,'s',marker='^',color='g', markersize=10,markeredgecolor = 'k')
     
     #Actualización de título
     plt.title('Segundos desde Origen: '+str(i))
@@ -98,7 +103,7 @@ plt.show()
 
 # save the animation as mp4 video file 
 writervideo = animation.FFMpegWriter(fps=2) 
-anim.save('Imagenes/eventG1.mp4', writervideo )
+anim.save('Imagenes/eventG2.mp4', writervideo )
 
 # save the animation as GIF file 
 # writergif = animation.PillowWriter(fps=10) 

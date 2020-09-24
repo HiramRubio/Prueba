feaedd8726346654deeb11f79d8136477ca0d355
HiramRubio <stevenrv97@gmail.com>
2020-09-24 05:54:08 -0600

    Update
    Texto con el nombre de las estaciones.
    Hace falta una opción para poder seleccionar el nombre de que estaciones plotear.

--- AnimatedTry.py
+++ AnimatedTry.py
@@ -21,6 +21,7 @@ def Animate_event(folder, homeDir):
     from mpl_toolkits.basemap import Basemap
     import math
     from stations_extractor import event_info_extractor
+    import numpy as np
     
     #Inicio de plot
     fig, ax = plt.subplots(figsize=(8,8))
@@ -48,18 +49,26 @@ def Animate_event(folder, homeDir):
           
     # initialization function 
     def init(): 
+        Names = ['XELA','STARS','CHAMP','CHULA','JUTI','SMARC']
         #Texto/Leyenda
         ax.plot(-89.00,17.0,'s',marker='^',color='r', markersize=6)
         ax.plot(-89.00,16.8,'s',marker='^',color='g', markersize=6)
-        ax.text(-88.90,16.97,'Onda P', fontsize=8)
-        ax.text(-88.90,16.77,'Onda S', fontsize=8)
+        ax.text(-88.90,16.97,'Onda P', fontsize=8,bbox=dict(boxstyle = "square",
+                  facecolor = "white"))
+        ax.text(-88.90,16.77,'Onda S', fontsize=8,bbox=dict(boxstyle = "square",
+                  facecolor = "white"))
         #Plot epicentro
         x,y = m(float(data[1]),float(data[0]))
         plot = ax.plot(x,y,'s',marker='*',color='k', markersize=10,markeredgecolor = 'k')
         #Plot de las estaciones sin color
-        for lat,lon in zip(est['Lat'],est['Lon']):
+        for lat,lon,name in zip(est['Lat'],est['Lon'],est['Est']):
             x,y = m(lon,lat)
             plot = ax.plot(x,y,'s',marker='^',color='none', markersize=10,markeredgecolor = 'k')
+            #Plot del nombre de algunas estaciones
+            if name in Names:
+                ax.text(x+0.01,y+0.07,name,fontsize=8,color='m',rotation=45,bbox=dict(boxstyle = "square",
+                  facecolor = "white"))
+                
         return plot
     
     # Estaciones a plotear
@@ -103,7 +112,7 @@ def Animate_event(folder, homeDir):
     ploted = []
     # call the animator     
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=t_max, interval=500)
-    #plt.show()
+    plt.show()
     
     #Decicimos guardar el vídeo 
     if(True):
@@ -120,7 +129,6 @@ def Animate_event(folder, homeDir):
     # writergif = animation.PillowWriter(fps=10) 
     # anim.save('random.gif', writer=writergif)
     
-#homeDir = "C:/Users/HRV/Desktop/Post-U/Trabajo/Prueba/Data/Eventos/"
-#folder = '2019-11-13-1628'
-
-#Animate_event(folder, homeDir)
+homeDir = "C:/Users/HRV/Desktop/Post-U/Trabajo/Prueba/Data/Eventos/"
+folder = '2019-11-13-1628'
+Animate_event(folder, homeDir)

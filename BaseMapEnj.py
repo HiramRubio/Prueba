e78f13bd397ac59c20bb59014bf55e61e8fd1013
HiramRubio <stevenrv97@gmail.com>
2020-12-11 03:09:58 -0600

    Update
    Cambios al mapa para enjambres

--- BaseMapEnj.py
+++ BaseMapEnj.py
@@ -19,9 +19,9 @@ import numpy as np
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
-fig, ax = plt.subplots(figsize=(8,8))
+fig, ax = plt.subplots(figsize=(12,8))
 #Leemos la data de los sismos
-dfs = pd.read_csv('Data/Informe.csv')
+dfs = pd.read_csv('Data/DataAV2.csv')
 Opc = False
 x1,x2 = max(dfs['lon']),min(dfs['lon'])
 y1,y2 = max(dfs['lat']),min(dfs['lat'])
@@ -43,10 +43,10 @@ x2=x2-0.02
 if(Opc==False):
     m = Basemap(resolution='i', # c, l, i, h, f or None
             projection='tmerc', 
-            lat_0=14.6569, lon_0=-90.51,
+            lat_0=15.94, lon_0=-90.7943,
             llcrnrlon=x2, llcrnrlat=y2,urcrnrlon=x1, urcrnrlat=y1, epsg=4326)
     
-    m.arcgisimage(service='Elevation/World_Hillshade', xpixels=800, dpi=100,verbose= True)
+    #m.arcgisimage(service='Elevation/World_Hillshade', xpixels=900, dpi=100,verbose= True)
     # m.drawmapboundary(fill_color='#46bcec')                  
     #m.fillcontinents(color='#f2f2f2',lake_color='#46bcec')
     # m.drawmapboundary(zorder = 0)
@@ -64,11 +64,11 @@ else:
     #m.arcgisimage(service='Elevation/World_Hillshade', xpixels=600, dpi=100,verbose= True)
 
 # draw parallels and meridians.
-parallels = np.arange(-92.,87.,0.1)
+parallels = np.arange(-92.,87.,0.5)
 # Label the meridians and parallels
 m.drawparallels(parallels,labels=[False,True,True,True])
 # Draw Meridians and Labels
-meridians = np.arange(-180.,181.,0.1)
+meridians = np.arange(-180.,181.,0.5)
 m.drawmeridians(meridians,labels=[True,True,False,True])
 
 #Creamos los colores para cada estacion 
@@ -80,9 +80,9 @@ if(Opc==False):
         ml = dfs['ml'][i]
         if(ml>4.0):     
             color='r' 
-            size=20
-            marker='*'
-        elif(ml>2.0):   
+            size=10
+            marker='o'
+        elif(ml>3.0):   
             color='y'
             size=8
             marker='o'
@@ -92,11 +92,12 @@ if(Opc==False):
             marker='o'
         m.plot(xpt,ypt,marker=marker,color=color,markersize=size,markeredgecolor='k',markeredgewidth=1)  # plot a dot 
 
+
 else:
-    x2=x2-0.1
-    x1 =x1+0.1
-    y2 =y2-0.1
-    y1 =y1+0.1
+    x2=x2-0.4
+    x1 =x1+0.4
+    y2 =y2-0.4
+    y1 =y1+0.4
     xn = np.linspace(x2,x1, 100)
     yn = np.linspace(y2,y1, 100)
     xc1 = np.linspace(x1,x1, 100)
@@ -109,9 +110,31 @@ else:
     m.plot(xc2,yn,marker=",",color="r") 
     #Pintamos solamente la capital
             
-#Leemos nuestra shapefile, no los activamos todos
+#Leemos nuestra shapefile
+#Delimitaciones de Guatemala
 m.readshapefile('Data/gtm/gtm_admbnda_adm0_ocha_conred_20190207', 'ej0',linewidth=1.5)
-m.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',drawbounds=False)
+m.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',drawbounds=True)
+#Cuerpos de Agua
+m.readshapefile('Data/shp/gis_osm_water_a_free_1', 'ca',drawbounds=True)
+m.readshapefile('Data/rios_gtm/rios_gtm', 'ca2',drawbounds=True)
+
+#Colocamos algunos municipios 
+xc,yc = m(-90.4067400,15.4689600)
+m.plot(xc,yc,marker ='*',color = 'k',markersize=20)
+xt,yt = m(-90.4067400+0.015,15.4689600+0.035)
+ax.text(xt,yt,'Coban', fontsize=10)
+
+xc,yc = m(-91.315506,15.804456)
+m.plot(xc,yc,marker ='*',color = 'k',markersize=20)
+xt,yt = m(-91.315506+0.02,15.804456+0.03)
+ax.text(xt,yt,'Santa Cruz Barillas', fontsize=10)
+
+xc,yc = m(-91.034722,15.487222)
+m.plot(xc,yc,marker ='*',color = 'k',markersize=20)
+xt,yt = m(-91.034722+0.02,15.487222+0.03)
+ax.text(xt,yt,'Chajul', fontsize=10)
+#bbox=dict(boxstyle = "square",facecolor = "white")
+
 
 if(Opc==True):
     patches   = []
@@ -125,8 +148,6 @@ if(Opc==True):
             patches.append( Polygon(np.array(shape), True) )      
     ax.add_collection(PatchCollection(patches, facecolor= '#527a7a', edgecolor='k', linewidths=1., zorder=2))
 
-m.readshapefile('Data/gtm/gtm_admbnda_adm1_ocha_conred_20190207', 'ej1',drawbounds=False) 
-m.readshapefile('Data/shp/gis_osm_water_a_free_1', 'ca',drawbounds=False)
 
 patches   = []
 for info, shape in zip(m.ca_info, m.ca):
@@ -134,8 +155,9 @@ for info, shape in zip(m.ca_info, m.ca):
 ax.add_collection(PatchCollection(patches, facecolor= '#85adad', edgecolor='w', linewidths=0.01, zorder=2))
 
 if(Opc==False): m.readshapefile('Data/gtm/gtm_admbnda_adm2_ocha_conred_20190207', 'ej2',drawbounds=False)
+
 #Titulo
-plt.title("Enjambre Sismico 2020-04-19 > 2020-04-21")
+plt.title("Sismicidad 2020-11-01 a 2020-12-10")
 #Guardar Imagen
-plt.savefig('Imagenes/Enjambre202004B.png', bbox_inches='tight')
+plt.savefig('Imagenes/Enjambre2020-11.png', bbox_inches='tight')
 plt.show()
